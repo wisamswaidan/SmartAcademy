6d6dd437aa115591117755d1b1fe0b3915c13e3c
Hema <ayhamnth@hotmail.com>
2019-06-03 16:45:45 +0200

    Delete Test class , user method delete and create work from wissam

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -6,9 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import static javafx.application.Application.*;
-
-
 public class Main extends Application {
 
     @Override
